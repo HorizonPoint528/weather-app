5025ed925c5faa044a6a9831212952fc6a4954fe
HorizonPoint528 <91239895+HorizonPoint528@users.noreply.github.com>
2024-01-23 11:08:39 -0500

    CSS styling

--- src/App.js
+++ src/App.js
@@ -24,22 +24,24 @@ function App() {
       <div className='search'>
         <input value = {location} onChange={event => setLocation(event.target.value)} placeholder='Enter Location' onKeyDown={searchLocation} type='text'></input>
       </div>
-      <div className='container'>
-        <div className = "top">
 
+      <div className='container'>
+        {data.name !== undefined &&
+          <div className = "top">
           <div className='location'>
-              <p>{data.name}</p>     
+              <h2>{data.name}</h2>     
           </div>
           <div className='temp'>
             {data.main ? <h1>{data.main.temp.toFixed()} ° F</h1> : null}
           </div>
           <div className='description'>
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.weather ? <h2>{data.weather[0].main}</h2> : null}
           </div>
         </div>
+        }
 
 
-        {data.name != undefined &&
+        {data.name !== undefined &&
         <div className='bottom'>
           <div className='feels'>
           {data.main ? <p className='bold'>{data.main.feels_like.toFixed()} ° F</p> : null}
